682035749c534970eae36f8edff46618e3dcaf29
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2025-03-11 19:18:18 +0530

    // write script steps

--- com.crm.SeleniumA11/src/Assignment/DwsLogin/DwsLoginPAge.java
+++ com.crm.SeleniumA11/src/Assignment/DwsLogin/DwsLoginPAge.java
@@ -6,17 +6,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DwsLoginPAge {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		String expectedTitle="Demo Web Shop";
+		//open the browser
 		ChromeDriver driver = new ChromeDriver();
+		//Maximize screen
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
+		//go on demowebshop website
 		driver.get("https://demowebshop.tricentis.com/");
+		// get actual title
 		String actualTitle = driver.getTitle();
+		//verification expectedtitle and actualtitle
 		if(expectedTitle.equals(actualTitle)) {
 			System.out.println("This is a Dws website");
+			// find element by text and click login page
 			driver.findElement(By.partialLinkText("Log")).click();
+			// find the element by name and set the value of email field
 			driver.findElement(By.name("Email")).sendKeys("tdeepak236@gmail.com");
+			// find the element by name and set the value of password field
 			driver.findElement(By.name("Password")).sendKeys("Deepak@1234");
+			// find the element by name and click login button
 			driver.findElement(By.className("login-button")).click();
 			System.out.println("Login Sucessfully");
 		}else {
--- com.crm.SeleniumA11/src/Assignment/VerifyDwsRegister/RegistrationPage.java
+++ com.crm.SeleniumA11/src/Assignment/VerifyDwsRegister/RegistrationPage.java
@@ -7,22 +7,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class RegistrationPage {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		String expectedUrl="https://demowebshop.tricentis.com/";
+		// open the browser
 		ChromeDriver driver = new ChromeDriver();
+		// maximize the browser
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
+		//go on demowebshop website
 		driver.get("https://demowebshop.tricentis.com/");
+		// get actual title
 		String actualUrl=driver.getCurrentUrl();
+		// validation between expected url and actual url
 		if(expectedUrl.equals(actualUrl)) {
 			System.out.println("I am in dws page");
-			WebElement registerPage = driver.findElement(By.partialLinkText("Register"));
-			registerPage.click();
+			// find the element by partialLinktext and click on register page
+			 driver.findElement(By.partialLinkText("Register")).click();
+			// find the element by id and click on male radio field
 			driver.findElement(By.id("gender-male")).click();
+			// set the fristName field
 			driver.findElement(By.id("FirstName")).sendKeys("Deepak");
+			// set the LastName field
 			driver.findElement(By.id("LastName")).sendKeys("Singh");
+			// set the email field
 			driver.findElement(By.id("Email")).sendKeys("tdeepak236@gmail.com");
+			// set the password field
 			driver.findElement(By.id("Password")).sendKeys("Deepak@1234");
+			// set the password field
 			driver.findElement(By.id("ConfirmPassword")).sendKeys("Deepak@1234");
+			// click on register button
 			driver.findElement(By.name("register-button")).click();
 			System.out.println("Registration Complete");
 		}else {
